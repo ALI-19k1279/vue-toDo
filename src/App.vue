cfd076ceff3192a7241121d753bb8818fec95632
Muhammad Ali Rehman <alirehman@pk.folio3.com>
2023-05-23 13:52:41 +0500

    added list rendering

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,9 @@
   <div id="app">
     <h1>To Do List</h1>
     <ul>
-      <li>
-        <to-do-item label="To Do Item" done="true"> </to-do-item>
+      <li v-for="item in ToDoItems" :key="item.id">
+        <to-do-item :label="item.label" :done="item.done" :id="item.id">
+        </to-do-item>
       </li>
     </ul>
   </div>
@@ -11,11 +12,20 @@
 
 <script>
 import ToDoItem from "./components/ToDoItem.vue";
-
+import uniqueId from "lodash.uniqueid";
 export default {
   name: "App",
   components: {
     ToDoItem,
+  },
+  data() {
+    return {
+      ToDoItems: [
+        { id: uniqueId("todo-"), label: "learn vue", done: false },
+        { id: uniqueId("todo-"), label: "learn vue2", done: false },
+        { id: uniqueId("todo-"), label: "learn vue3", done: true },
+      ],
+    };
   },
 };
 </script>
--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,14 +1,13 @@
 <script>
-import uniqueId from "lodash.uniqueid";
 export default {
   props: {
     label: { required: true, type: String },
     done: { default: false, type: Boolean },
+    id: { required: true, type: String },
   },
   data() {
     return {
       isDone: this.done,
-      id: uniqueId("todo-"),
     };
   },
 };
