60fcf63cb873b39c3d269ad6248eaf734d24c337
Muhammad Ali Rehman <alirehman@pk.folio3.com>
2023-05-23 15:01:42 +0500

    added emitter for adding to-do item and styling

---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <form @submit.prevent="onSubmit">
+      <h2 class="label-wrapper">
+        <label for="to-do-input" class="label__lg">
+          What needs to be done?
+        </label>
+      </h2>
+      <input
+        type="text"
+        id="to-do-input"
+        name="new-todo"
+        autocomplete="off"
+        class="input__lg"
+        v-model.lazy.trim="label"
+      />
+      <button type="submit" class="btn btn__primary btn__lg">Add</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onSubmit() {
+      if (this.label === "") {
+        return;
+      }
+      this.$emit("todo-added", this.label);
+      this.label = "";
+    },
+  },
+  data() {
+    return {
+      label: "",
+    };
+  },
+};
+</script>
